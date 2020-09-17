9a5983ece1acb6092a27484b1f7377c86f4f24ad
Ramsho Pemmaraju <ramshopemmaraju@Ramshos-MacBook-Air.local>
2020-09-17 18:05:37 +0530

    REACT-ADDED

--- API/Startup.cs
+++ API/Startup.cs
@@ -33,6 +33,13 @@ namespace API
             {
                 opt.UseSqlite(Configuration.GetConnectionString("DefaultConnection"));
             });
+            services.AddCors(opt => 
+            {
+                opt.AddPolicy("CorsPolicy", policy =>
+                {
+                    policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000");
+                });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -49,10 +56,14 @@ namespace API
 
             app.UseAuthorization();
 
+            app.UseCors("CorsPolicy");
+            
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
             });
+
+            
         }
     }
 }
