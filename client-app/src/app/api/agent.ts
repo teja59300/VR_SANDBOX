31a487e594c8ec96490e462519c64f5accee405a
teja5930 <pemmarajus_10217@tep.isb.edu>
2020-10-29 14:37:24 +0530

    end of section 13 (client side login  and registration

--- client-app/src/app/api/agent.ts
+++ client-app/src/app/api/agent.ts
@@ -1,31 +1,34 @@
 import axios, { AxiosResponse } from 'axios';
 import { IActivity } from '../models/activity';
-import {history} from '../..';
+import { history } from '../..';
 import { toast } from 'react-toastify';
+import { IUser, IUserFormValues } from '../models/user';
 
 axios.defaults.baseURL = 'http://localhost:5000/api';
 
-axios.interceptors.response.use(undefined,error =>{
-    if(error.message === 'Network Error' && !error.response)
-    {
-        toast.error('Network error - make sure API is running');
+axios.interceptors.request.use((config) => {
+    const token = window.localStorage.getItem('jwt');
+    if (token) config.headers.Authorization = `Bearer ${token}`;
+    return config;
+}, error => {
+    return Promise.reject(error);
+})
+
+axios.interceptors.response.use(undefined, error => {
+    if (error.message === 'Network Error' && !error.response) {
+        toast.error('Network error - make sure API is running!')
     }
-    //console.log(error);
-    const {status,data,config}= error.response;
-   // console.log(error.response);
-   if(error.response.status === 404)
-   {
-       history.push('/notfound');
-   }
-   if(status === 400 && config.method=== 'get' && data.errors.hasOwnProperty('id'))
-   {
-       history.push('/notfound');
-   }
-   if(status === 500)
-   {
-       toast.error('Server error - check the terminal for more info');
-   }
-   throw error;
+    const {status, data, config} = error.response;
+    if (status === 404) {
+        history.push('/notfound')
+    }
+    if (status === 400 && config.method === 'get' && data.errors.hasOwnProperty('id')) {
+        history.push('/notfound')
+    }
+    if (status === 500) {
+        toast.error('Server error - check the terminal for more info!')
+    }
+    throw error.response;
 })
 
 const responseBody = (response: AxiosResponse) => response.data;
@@ -48,6 +51,13 @@ const Activities = {
     delete: (id: string) => requests.del(`/activities/${id}`)
 }
 
+const User = {
+    current: (): Promise<IUser> => requests.get('/user'),
+    login: (user: IUserFormValues): Promise<IUser> => requests.post(`/user/login`, user),
+    register: (user: IUserFormValues): Promise<IUser> => requests.post(`/user/register`, user),
+}
+
 export default {
-    Activities
+    Activities,
+    User
 }
--- client-app/src/features/activities/form/ActivityForm.tsx
+++ client-app/src/features/activities/form/ActivityForm.tsx
@@ -2,7 +2,6 @@ import React, { useState, useContext, useEffect } from 'react';
 import { Segment, Form, Button, Grid } from 'semantic-ui-react';
 import {   ActivityFormValues } from '../../../app/models/activity';
 import { v4 as uuid } from 'uuid';
-import ActivityStore from '../../../app/stores/activityStore';
 import { observer } from 'mobx-react-lite';
 import { RouteComponentProps } from 'react-router';
 import {Form as FinalForm,Field} from 'react-final-form';
@@ -13,6 +12,7 @@ import { status } from '../../../app/common/options/statusOptions';
 import DateInput from '../../../app/common/form/DateInput';
 import { combineDateAndTime } from '../../../app/common/util/util';
 import {combineValidators, isRequired, composeValidators, hasLengthGreaterThan} from 'revalidate';
+import { RootStoreContext } from '../../../app/stores/rootStore';
 
 interface DetailParams {
   id: string;
@@ -38,13 +38,13 @@ const ActivityForm: React.FC<RouteComponentProps<DetailParams>> = ({
   match,
   history
 }) => {
-  const activityStore = useContext(ActivityStore);
+  const rootStore = useContext(RootStoreContext);
   const {
     createActivity,
     editActivity,
     submitting,
     loadActivity,
-  } = activityStore;
+  } = rootStore.activityStore;
 
   const [activity, setActivity] = useState(new ActivityFormValues());
 
